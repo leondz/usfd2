505ecf77eeb674e6679cb99966e18172e5507411
leon@dcs.shef.ac.uk <leon@dcs.shef.ac.uk@c5fca692-efb5-c971-8805-f1b4baf0a226>
2010-07-23 13:09:49 +0000

    moved timex normalisation code out of tempeval script; updated ruleset to include e.g. 10th january 1920

--- trunk/a.py
+++ trunk/a.py
@@ -1,7 +1,7 @@
 import re
-from datetime import date,  timedelta
 import tempEval2
-from timexIdentify import findTimexes,  buildSentenceList,  getTimexType
+from datetime import date
+from timexIdentify import findTimexes,  buildSentenceList,  getTimexType,  getTimexValue
 
 
 def findSubstrings(text, substring):
@@ -17,18 +17,13 @@ def findSubstrings(text, substring):
  
 
 
-segmentationFile = '/home/leon/time/tempeval2/test/english/entities/base-segmentation.tab'
-dctFile = '/home/leon/time/tempeval2/test/english/dct.txt'
+segmentationFile = '/home/leon/time/tempeval2/eval/entities/base-segmentation.tab'
+dctFile = '/home/leon/time/tempeval2/eval/dct.txt'
 extentsFile = 'timex-extents.tab'
 attribsFile = 'timex-attributes.tab'
 
-futureLimit = 14
-
 
 words = tempEval2.readSegmentation(segmentationFile)
-
-
-
 
 timexes = findTimexes(words)
 
@@ -57,7 +52,6 @@ for line in file:
 
 
 attrs = []
-numbersRx = re.compile(r'^[0-9]+ ')
 
 for t in timexes:
     sentenceList = buildSentenceList(words[t['doc']][t['sentence']])
@@ -68,109 +62,10 @@ for t in timexes:
 
     timexType = getTimexType(timexString,  previous3Words)
     
-    timexValue = None
-    if timexString.lower == 'today' or timexString.lower == 'now':
-        timexValue = 'PRESENT_REF'
-    
-    timexValueComplete = False
-    timexValue = 'P'
-    
-    distance = ''
-    if timexString.lower().find('one ') != -1 or timexString.lower().find('next ') != -1:
-        distance = '1'
-    elif timexString.lower().find('two ') != -1:
-        distance = '2'
-    elif timexString.lower().find('three ') != -1:
-        distance = '3'
-    elif timexString.lower().find('four ') != -1:
-        distance = '4'
-    elif timexString.lower().find('five ') != -1:
-        distance = '5'
-    elif timexString.lower().find('six ') != -1:
-        distance = '6'
-    elif timexString.lower().find('seven ') != -1:
-        distance = '7'
-    elif timexString.lower().find('eight ') != -1:
-        distance = '8'
-    elif timexString.lower().find('nine ') != -1:
-        distance = '9'
-    elif timexString.lower().find('ten ') != -1:
-        distance = '10'
-    elif timexString.lower().find('last ') != -1 or timexString.lower().find('earlier') != -1:
-        distance = -1
-    else:
-        distance = 'X'
-    
-    if timexString.lower().find('few ') != -1:
-        distance = -1
-    
-    if distance and distance != 'X' and int(distance) > 0 and (timexString.lower().find('earlier') > -1 or timexString.lower().find('ago') > -1):
-        distance = str(-int(distance))
-    
-    period = ''
-    if not timexValueComplete:
-        if timexString.lower().find('year') != -1:
-            period = 'Y'
-        elif timexString.lower().find('quarter') != -1:
-            period = 'Q'
-        elif timexString.lower().find('month') != -1:
-            period = 'M'
-        elif timexString.lower().find('week') != -1:
-            period = 'W'
-        elif timexString.lower().find('day') != -1:
-            period = 'D'
-        elif timexString.lower().find('hour') != -1:
-            period = 'H'
-        else:
-            period = 'X'
+    docDate = dcts[t['doc']]
+    timexValue = getTimexValue(timexString,  timexType,  docDate)
 
-    m = numbersRx.match(timexString)
-    if m:
-        distance = m.group()[:-1]
-
-    if distance == -1 and timexType == 'DURATION':
-        if timexString[-1:] == 's':
-            distance = 'X'
-        else:
-            distance = '1'
-
-    if timexType == 'DATE':
-        timexDate = dcts[t['doc']]
-        
-        if timexString[0:3].lower() in months:
-            
-            if timexString == 'may': # if not capitalised, ignore it
-                continue
-            
-            timexMonth = timexString[0:3].lower();
-            if timexString.find(' ') != -1:
-                _x,  dayNumber = timexString.split(' ')
-                timexDate = timexDate.replace(month=(months.index(timexMonth)+1))
-                timexDate = timexDate.replace(day=int(dayNumber))
-            else:
-                period = 'M'
-            
-            daysInFuture = (timexDate - dcts[t['doc']]).days
-            if daysInFuture > futureLimit:
-                timexDate = timexDate.replace(year = timexDate.year - 1)
-
-        elif distance and distance != 'X':
-            distance = int(distance)
-            if period == 'Y':
-                timexDate += timedelta(days = 365*distance)
-            elif period == 'Q':
-                timexDate += timedelta(days = 90*distance)
-            elif period == 'M':
-                timexDate += timedelta(days = 30*distance)
-            elif period == 'W':
-                timexDate += timedelta(weeks = 1*distance)
-            elif period == 'D':
-                timexDate += timedelta(days = 1*distance)
-            elif period == 'H':
-                timexDate += timedelta(hours = 1*distance)
-    
-    
-    # add one entry per attribute    
+    # add one entry (for output file) per attribute
     if timexType:
         t['attr'] = 'type'
         t['value'] = timexType
@@ -178,15 +73,6 @@ for t in timexes:
     
     if timexValue:
         
-        if timexType == 'DURATION':
-            timexValue = 'P' + distance + period
-        elif timexType == 'DATE':
-            if period == 'Y':
-                timexValue = timexDate.year
-            elif period in ['Q',  'M']:
-                timexValue = '-'.join(map(str, [timexDate.year, '%02d' % (timexDate.month)]))
-            else:
-                timexValue = timexDate
         
         t['attr'] = 'value'
         t['value'] = str(timexValue)
