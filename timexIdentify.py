4165d42a62253d6669ea471fc7c3f2b5633c3fe1
leon@dcs.shef.ac.uk <leon@dcs.shef.ac.uk@c5fca692-efb5-c971-8805-f1b4baf0a226>
2010-07-23 13:09:49 +0000

    moved timex normalisation code out of tempeval script; updated ruleset to include e.g. 10th january 1920

--- timexIdentify.py
+++ timexIdentify.py
@@ -1,5 +1,9 @@
 import re
+from datetime import date,  timedelta
 
+numbersRx = re.compile(r'^[0-9]+ ')
+months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+futureLimit = 14
 
 def addBoundary(string):
     return '^' + string + '$'
@@ -39,7 +43,6 @@ def buildSentenceList(sentenceDict): # convert a sentence dictionary including c
 
 
 def findTimexes(words):  # expects words to be in the format words[filename][sentence index][word index] = "token" .
-    months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
 
     calendar_interval = "(minute|hour|day|weekend|week|month|quarter|year)"
     longdays = "((mon|tues|wednes|thurs|fri|satur|sun|to|yester)day|tomorrow)"
@@ -180,6 +183,126 @@ def getTimexType(timexString,  previous3Words):
         timexType = 'DATE'
     
     return timexType
+
+def getTimexValue(timexString,  timexType,  docDate):
+
+    timexValue = None
+    if timexString.lower == 'today' or timexString.lower == 'now':
+        timexValue = 'PRESENT_REF'
+    
+    timexValueComplete = False
+    timexValue = 'P'
+    
+    distance = ''
+    if timexString.lower().find('one ') != -1 or timexString.lower().find('next ') != -1:
+        distance = '1'
+    elif timexString.lower().find('two ') != -1:
+        distance = '2'
+    elif timexString.lower().find('three ') != -1:
+        distance = '3'
+    elif timexString.lower().find('four ') != -1:
+        distance = '4'
+    elif timexString.lower().find('five ') != -1:
+        distance = '5'
+    elif timexString.lower().find('six ') != -1:
+        distance = '6'
+    elif timexString.lower().find('seven ') != -1:
+        distance = '7'
+    elif timexString.lower().find('eight ') != -1:
+        distance = '8'
+    elif timexString.lower().find('nine ') != -1:
+        distance = '9'
+    elif timexString.lower().find('ten ') != -1:
+        distance = '10'
+    elif timexString.lower().find('last ') != -1 or timexString.lower().find('earlier') != -1:
+        distance = -1
+    else:
+        distance = 'X'
+    
+    if timexString.lower().find('few ') != -1:
+        distance = -1
+    
+    if distance and distance != 'X' and int(distance) > 0 and (timexString.lower().find('earlier') > -1 or timexString.lower().find('ago') > -1):
+        distance = str(-int(distance))
+    
+    period = ''
+    if not timexValueComplete:
+        if timexString.lower().find('year') != -1:
+            period = 'Y'
+        elif timexString.lower().find('quarter') != -1:
+            period = 'Q'
+        elif timexString.lower().find('month') != -1:
+            period = 'M'
+        elif timexString.lower().find('week') != -1:
+            period = 'W'
+        elif timexString.lower().find('day') != -1:
+            period = 'D'
+        elif timexString.lower().find('hour') != -1:
+            period = 'H'
+        else:
+            period = 'X'
+
+    m = numbersRx.match(timexString)
+    if m:
+        distance = m.group()[:-1]
+
+    if distance == -1 and timexType == 'DURATION':
+        if timexString[-1:] == 's':
+            distance = 'X'
+        else:
+            distance = '1'
+
+    if timexType == 'DATE':
+        
+        timexDate = docDate
+        
+        if timexString[0:3].lower() in months:
+            
+            if timexString == 'may': # if not capitalised, ignore it
+                return
+            
+            timexMonth = timexString[0:3].lower();
+            if timexString.find(' ') != -1:
+                _x,  dayNumber = timexString.split(' ')
+                timexDate = timexDate.replace(month=(months.index(timexMonth)+1))
+                timexDate = timexDate.replace(day=int(dayNumber))
+            else:
+                period = 'M'
+            
+            daysInFuture = (timexDate - docDate).days
+            if daysInFuture > futureLimit:
+                timexDate = timexDate.replace(year = timexDate.year - 1)
+
+        elif distance and distance != 'X':
+            distance = int(distance)
+            if period == 'Y':
+                timexDate += timedelta(days = 365*distance)
+            elif period == 'Q':
+                timexDate += timedelta(days = 90*distance)
+            elif period == 'M':
+                timexDate += timedelta(days = 30*distance)
+            elif period == 'W':
+                timexDate += timedelta(weeks = 1*distance)
+            elif period == 'D':
+                timexDate += timedelta(days = 1*distance)
+            elif period == 'H':
+                timexDate += timedelta(hours = 1*distance)
+    
+    
+    if timexValue:
+        if timexType == 'DURATION':
+            timexValue = 'P' + distance + period
+        elif timexType == 'DATE':
+            if period == 'Y':
+                timexValue = timexDate.year
+            elif period in ['Q',  'M']:
+                timexValue = '-'.join(map(str, [timexDate.year, '%02d' % (timexDate.month)]))
+            else:
+                timexValue = timexDate
+
+    return timexValue
+
+
 
 def annotateTimexesKBP(inputString,  tag="timex"):
     # return a list off start/finish offset pairs; []{start_char=xx, end_char=yy}
